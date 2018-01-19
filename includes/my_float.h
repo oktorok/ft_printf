be272d452133da3136b21ba91f466706eabf89db
Mateo RODRIGUEZ <mrodrigu@e1r7p11.42.fr>
2018-01-19 18:58:11 +0100

    cosas del float

---
+++ includes/my_float.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   my_float.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/19 18:41:29 by mrodrigu          #+#    #+#             */
+/*   Updated: 2018/01/19 18:54:23 by mrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MY_FLOAT_H
+# define MY_FLOAT_H
+
+# include "ten_power.h"
+
+int		ft_choose_power(int e, int alpha);
+double	ft_sige(double x);
+
+
+#endif
