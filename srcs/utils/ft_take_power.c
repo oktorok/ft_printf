cfc31047ebc8d9508a9595cb5a4ab3f71e0964c7
Mateo RODRIGUEZ <mrodrigu@e1r9p13.42.fr>
2018-01-22 18:21:18 +0100

    float3-1

--- srcs/utils/ft_take_power.c
+++ srcs/utils/ft_take_power.c
@@ -6,19 +6,19 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/19 18:31:00 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/01/22 03:42:20 by mrodrigu         ###   ########.fr       */
+/*   Updated: 2018/01/22 17:54:40 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/libftprintf.h"
-#include <stdio.h>
+
 t_myfloat	ft_take_power(int k)
 {
 	t_myfloat	res;
-	int index = 343 + k;
+	int			index;
+
+	index = 343 + k;
 	res.mantissa = powers_ten[index];
-	printf ("El exponente de 10 es: %lu\n",res.mantissa);
-	res.exponent = powers_ten_e[index] ;
-	printf ("El exponente de 10 es: %i\n",res.exponent);
-	return res;
+	res.exponent = powers_ten_e[index];
+	return (res);
 }
