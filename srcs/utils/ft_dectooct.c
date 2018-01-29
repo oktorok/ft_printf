3ce73cf3c0622f1a3ce9299a5b4c71187bf1b835
Mateo RODRIGUEZ <mrodrigu@e1r10p13.42.fr>
2018-01-29 23:06:37 +0100

    octal arreglado

--- srcs/utils/ft_dectooct.c
+++ srcs/utils/ft_dectooct.c
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/25 23:11:18 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/01/27 00:52:56 by jagarcia         ###   ########.fr       */
+/*   Updated: 2018/01/29 23:04:11 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,27 +36,23 @@ char	*ft_octsize(char *n, size_t len, size_t *s_len)
 	return (ft_strnew(sizeof(char) * (*s_len)));
 }
 
-size_t	ft_rd1(char *n, size_t i, char *str, size_t s_len)
+size_t	ft_rd1(char *n, size_t i, size_t *j)
 {
-	str[s_len--] = (n[i] & 0x7) + 48;
-	str[s_len--] = ((n[i] & 0x38) >> 3) + 48;
-	return (s_len);
-}
-
-size_t	ft_rd2(char *n, size_t i, char *str, size_t s_len)
-{
-	str[s_len--] = (((n[i] & 0x1) << 2) + ((n[i - 1] & 0xC0) >> 6)) + 48;
-	str[s_len--] = ((n[i] & 0xE) >> 1) + 48;
-	str[s_len--] = ((n[i] & 0x70) >> 4) + 48;
-	return (s_len);
-}
-
-size_t	ft_rd3(char *n, size_t i, char *str, size_t s_len)
-{
-	str[s_len--] = (((n[i] & 0x3) << 1) + ((n[i - 1] & 0x80) >> 7)) + 48;
-	str[s_len--] = ((n[i] & 0x1C) >> 2) + 48;
-	str[s_len--] = ((n[i] & 0xE0) >> 5) + 48;
-	return (s_len);
+	if ((i % 8) == 0)
+		return ((n[*j] & 0x7) + 48);
+	if ((i % 8) == 1)
+		return (((n[(*j)++] & 0x38) >> 3) + 48);
+	if ((i % 8) == 2)
+		return ((((n[*j] & 0x1) << 2) + ((n[(*j) - 1] & 0xC0) >> 6)) + 48);
+	if ((i % 8) == 3)
+		return (((n[*j] & 0xE) >> 1) + 48);
+	if ((i % 8) == 4)
+		return (((n[(*j)++] & 0x70) >> 4) + 48);
+	if ((i % 8) == 5)
+		return ((((n[*j] & 0x3) << 1) + ((n[(*j) - 1] & 0x80) >> 7)) + 48);
+	if ((i % 8) == 6)
+		return (((n[*j] & 0x1C) >> 2) + 48);
+	return (((n[(*j)++] & 0xE0) >> 5) + 48);
 }
 
 char	*ft_dectooct(void *num, size_t len)
@@ -65,24 +61,15 @@ char	*ft_dectooct(void *num, size_t len)
 	char	*str;
 	size_t	i;
 	size_t	s_len;
+	size_t	j;
 
 	n = (char *)num;
 	if (!(str = ft_octsize(n, len, &s_len)))
 		return (NULL);
 	i = 0;
+	j = 0;
 	s_len--;
-	while (i < len)
-	{
-		s_len = ft_rd1(n, i, str, s_len);
-		if (++i < len)
-			s_len = ft_rd2(n, i, str, s_len);
-		if (++i < len)
-			s_len = ft_rd3(n, i, str, s_len);
-		i++;
-	}
-	if ((((len * 8) % 24) % 3) == 1)
-		str[0] = ((n[len - 1] & 0x80) >> 7) + 48;
-	else if ((((len * 8) % 24) % 3) == 2)
-		str[0] = ((n[len - 1] & 0xC0) >> 6) + 48;
+	while ((s_len + 1) > 0)
+		str[s_len--] = ft_rd1(n, i++, &j);
 	return (str);
 }
