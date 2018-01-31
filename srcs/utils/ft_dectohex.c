cfbf3c5bfd1321234349468d6843540636014534
Mateo RODRIGUEZ <mrodrigu@e1r8p10.42.fr>
2018-01-31 23:41:07 +0100

    ft_dectohex

--- srcs/utils/ft_dectohex.c
+++ srcs/utils/ft_dectohex.c
@@ -6,7 +6,7 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/26 01:08:05 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/01/31 17:35:30 by jagarcia         ###   ########.fr       */
+/*   Updated: 2018/01/31 10:07:50 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,54 +14,55 @@
 
 char	*ft_hexsize(char *n, size_t len, size_t *s_len)
 {
-//	size_t	i;
-	int		i;
-	char	j;
+	long	i;
+	size_t	j;
+	size_t	k;
 
 	i = len - 1;
 	j = 0;
-	while (!(n[i] & (0xF0 >> j)))
+	k = 0;
+	while (j < len)
 	{
-		j += 4;
-		if (j == 8)
-		{
-			(i)--;
-			(j) = 0;
-		}
+		if (!(n[i] & 0xF0))
+			k++;
+		else
+			break ;
+		if (!(n[i] & 0xF))
+			k++;
+		else
+			break ;
+		i--;
+		j++;
 	}
-//////////////////////
-	if (i < 0)
-		i = 0;
-/////////////////////
-	*s_len = (((len * 8) - ((((len - 1) - i) * 8) + (j))) / 4) - 1;
-	return (ft_strnew(sizeof(char) * (*s_len + 1)));
+	*s_len = ((len * 2) - k) ? ((len * 2) - k) : 1;
+	return (ft_strnew(sizeof(char) * (*s_len)));
 }
 
 char	*ft_dectohex(void *num, size_t len)
 {
-	char	*n;
+	size_t	k;
 	char	*str;
 	size_t	i;
 	size_t	s_len;
-
-	n = (char *)num;
-	if (!(str = ft_hexsize(n, len, &s_len)))
+	size_t	aux;
+	
+	if (!(str = ft_hexsize((char *)num, len, &s_len)))
 		return (NULL);
 	i = 0;
-	while (i < len)
+	aux = s_len - 1;
+	k = 0;
+	while (i < s_len)
 	{
-		str[s_len] = n[i] & 0xF;
-		str[s_len - 1] = (n[i] & 0xF0) >> 4;
-		if (str[s_len] >= 10)
-			str[s_len] = str[s_len] + 'a' - 10;
+		str[aux] = (((char *)num)[i] & (0xF << (4 * k))) >> (4 * k);
+		if (str[aux] >= 10)
+			str[aux] = str[aux] + 'a' - 10;
 		else
-			str[s_len] = str[s_len] + 48;
-		if (str[s_len - 1] >= 10)
-			str[s_len - 1] = str[s_len - 1] + 'a' - 10;
+			str[aux] = str[aux] + 48;
+		aux--;
+		if (k--)
+			i++;
 		else
-			str[s_len - 1] = str[s_len - 1] + 48;
-		s_len -= 2;
-		i++;
+			k = 1;
 	}
 	return (str);
 }
