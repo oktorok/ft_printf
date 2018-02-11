bd1a8f0bd469c4304a94c48a5932a32f1d5cc0d0
latiagertrutis <teorodrip@hotmail.com>
2018-02-11 07:00:32 +0100

    pt the point arreglado

--- prueba2.c
+++ prueba2.c
@@ -6,7 +6,7 @@
 /*   By: jagarcia <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/05 21:52:57 by jagarcia          #+#    #+#             */
-/*   Updated: 2018/02/10 07:32:11 by jagarcia         ###   ########.fr       */
+/*   Updated: 2018/02/10 19:16:03 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,7 +82,8 @@ char	cosa[100] = {12, 255, 42, 12, 89, 45, 34, 96};
 char	str2[200] = {0};
 //ft_printf("Tu %b Madre", 255);
 unsigned int gete = -42;
-T= printf("%a\n%1$e\n%x\n",4.3,43); 
+T= printf("%f\n", 0.00002); 
+T= ft_printf("%f\n", 0.00002); 
 //T= ft_printf("%c\n",L'\x23CD'); 
 //ft_putchar('<');
 //ft_putnbr(T);
--- srcs/utils/utils_float/ft_putthepoint.c
+++ srcs/utils/utils_float/ft_putthepoint.c
@@ -6,27 +6,29 @@
 /*   By: mrodrigu <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/02/07 05:20:16 by mrodrigu          #+#    #+#             */
-/*   Updated: 2018/02/10 06:30:43 by jagarcia         ###   ########.fr       */
+/*   Updated: 2018/02/10 19:14:03 by mrodrigu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libftprintf.h"
+#include "../../../includes/libftprintf.h"
 
 char	*ft_putthepoint(char *str, int *siz_cuant)
 {
 	char	*new;
-	size_t	pos;
-
+	int		pos;
 	pos = (ft_strchr(str, 'e') - str - 1) + ft_atoi(ft_strchr(str, 'e') + 1);
-	if (!(new = ft_strnew(ft_strlen(str) + 1)))
+	if (!(new = ft_strnew(ft_strlen(str) + 1 + ((pos < 0) ? (-pos) : 0))))
 		return (NULL);
-	ft_strncpy(new, str, pos + 1);
-	new[pos + 1] = '.';
-	ft_strcpy(new + pos + 2, str + pos + 1);
-
-
+	if (pos < 0)
+		ft_memset(new, '0', -pos + 1);
+	else
+		ft_strncpy(new, str, pos + 1);
+	new[(pos < 0) ? 1 : (pos + 1)] = '.';
+	ft_strcpy(new + ((pos < 0) ? (-pos + 1) : (pos + 2)), str + ((pos < 0) ? 0 : (pos + 1)));
 	///
 	new = ft_strsub(new, 0, ft_strchr(new, '.') - new + siz_cuant[1]);
+	ft_putstr(new);
+	ft_putstr("\n");
 	///
 	return (new);
 }
