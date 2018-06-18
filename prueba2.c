0cb7e52376f40d5d483327e1e6503c58cc2d1512
Jaume GARCIA <jagarcia@e1r9p8.42.fr>
2018-06-18 12:50:00 +0200

    prueba anyadido

--- prueba2.c
+++ prueba2.c
@@ -1,11 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   prueba2.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jagarcia <mrodrigu@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/06/18 12:34:52 by jagarcia          #+#    #+#             */
+/*   Updated: 2018/06/18 12:48:16 by jagarcia         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libftprintf.h"
 #include <stdio.h>
 
-int main(void)
+int	main(void)
 {
-//	char f[10] = {1, 3, 5, 7, 10, 48};
-	ft_printf("\n%5$c, %4$.*3$s, %2$*6$s, %1$*7$s\n","1 arg", "2 arg", 3, "4 arg", '5', 10 ,40);
-	printf("\n%5$c, %4$.*3$s, %2$*6$s, %1$*7$s\n","1 arg", "2 arg", 3, "4 arg", '5', 10, 40);
-//	printf("\n%g\n",f);
-//	2.147484e+09
+	char *buffer;
+	char *frase;
+	int numero;
+	
+	buffer = "%i, %2$.*1$s|\n";
+	frase = "caca pedo pis";
+	numero = 4;
+	
+	ft_putstr("PRINTF:\n");
+	printf(buffer, numero, frase, numero + 5);
+	ft_putstr("FT_PRINTF\n");
+	ft_printf(buffer, numero, frase, numero + 5);
 }
